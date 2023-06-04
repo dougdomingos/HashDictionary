aa75c73596847c8b67f9d62559b4dd3a3890f5ee
Douglas Domingos <doug.domingossilva@gmail.com>
2023-06-04 09:05:16 -0300

    adiciona funções de Hash do dicionário

--- HashTable.py
+++ HashTable.py
@@ -3,9 +3,10 @@ from utils.init_table import init_table
 class HashTable:
     def __init__(self, size):
         self.table = init_table(size)
+        self.size = size
     
-    def insert(self, value: str):
-        hash_code = ord(value[0]) % len(self.table)
+    def insert(self, value: str, hash_func: callable):
+        hash_code = hash_func(value, self.size)
 
         self.table[hash_code].append(value)
     
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import string
 from HashTable import HashTable
 
+from hashes.word_hash import one_letter_hash
+
 table = HashTable(26)
 for letter in string.ascii_lowercase:
-    table.insert(letter)
-
+    table.insert(letter, one_letter_hash)
 
 print(table)
